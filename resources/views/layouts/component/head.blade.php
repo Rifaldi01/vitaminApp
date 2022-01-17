00b155936526892419d546191926f251918f0b5d
Rifaldi01 <rifaldipratama2004@gmail.com>
2022-01-17 07:40:33 -1100

    vitacimin

---
+++ resources/views/layouts/component/head.blade.php
@@ -0,0 +1,14 @@
+<head>
+    <title>Furnish - Free Bootstrap 5 Template by Colorlib</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="preconnect" href="https://fonts.gstatic.com/">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700;800;900&amp;display=swap"
+          rel="stylesheet">
+    <link rel="stylesheet"
+          href="{{URL::to('asstes/stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css')}}">
+    <link rel="stylesheet"
+          href="{{URL::to('assets/cdnjs.cloudflare.com/ajax/libs/ionicons/4.5.6/css/ionicons.min.css')}}">
+    <link rel="stylesheet"
+          href="{{URL::to('assets/css/A.animate.css%2bflaticon.css%2btiny-slider.css%2bglightbox.min.css%2baos.css%2bdatepicker.min.css%2bstyle.css%2cMcc.9FRStqPSTJ.css.pagespeed.cf.bdTt0htMes.css')}}"/>
+</head>
---
+++ resources/views/layouts/component/js.blade.php
@@ -0,0 +1,27 @@
+<script src="{{URL::to('assets/js/bootstrap.bundle.min.js')}}"></script>
+<script src="{{URL::to('assets/js/tiny-slider.js')}}"></script>
+<script src="{{URL::to('assets/js/glightbox.min.js%2baos.js.pagespeed.jc.PASSSfJlwt.js')}}"></script>
+<script>eval(mod_pagespeed_GoxUyftWQ2);</script>
+<script>eval(mod_pagespeed_EpxrUfmyQz);</script>
+<script src="{{URL::to('assets/js/datepicker.min.js%2bgoogle-map.js%2bmain.js.pagespeed.jc.USRLwWRXS3.js')}}"></script>
+<script>eval(mod_pagespeed_J_izKJnS9b);</script>
+<script
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&amp;sensor=false"></script>
+<script>eval(mod_pagespeed_udx4xrxpoc);</script>
+<script>eval(mod_pagespeed_RGbNin9rny);</script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-23581568-13"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+
+    gtag('js', new Date());
+
+    gtag('config', 'UA-23581568-13');
+</script>
+<script defer src="https://static.cloudflareinsights.com/beacon.min.js/v64f9daad31f64f81be21cbef6184a5e31634941392597"
+        integrity="sha512-gV/bogrUTVP2N3IzTDKzgP0Js1gg4fbwtYB6ftgLbKQu/V8yH2+lrKCfKHelh4SO3DPzKj4/glTO+tNJGDnb0A=="
+        data-cf-beacon='{"rayId":"6b6aef94895fdcea","token":"cd0b4b3a733644fc843ef0b185f98241","version":"2021.11.0","si":100}'
+        crossorigin="anonymous"></script>
